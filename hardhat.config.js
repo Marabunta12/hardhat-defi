410b853aee5700fea49a4e4d4dfdf9c90bc050cd
Bartek <s210869@sggw.edu.pl>
2022-08-25 16:54:53 +0200

    added depositing collateral

---
+++ hardhat.config.js
@@ -0,0 +1,63 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
+            blockConfirmations: 1,
+        },
+        rinkeby: {
+            chainId: 4,
+            blockConfirmations: 6,
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+        localhost: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            rinkeby: ETHERSCAN_API_KEY,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    }, // 0.4.19
+    solidity: {
+        compilers: [{ version: "0.6.12" }, { version: "0.4.19" }],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 300000,
+    },
+};
